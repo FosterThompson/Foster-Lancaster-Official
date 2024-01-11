a5fda58549c2c0527361180bb0b8b978f6c3cfca
Foster Thompson <foster@fosterlancastermusic.com>
2024-01-10 19:39:14 -0500

    Initial Commit

---
+++ Foster Lancaster Official/Foster_Lancaster_OfficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foster_Lancaster_OfficialApp.swift
+//  Foster Lancaster Official
+//
+//  Created by Foster Lancaster on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Foster_Lancaster_OfficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
